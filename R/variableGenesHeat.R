b139b7c69d9e6314520c6290c208e4d043a5294c
perllb <per_ludvik.brattaas@med.lu.se>
2017-11-03 17:31:43 +0100

    add mostVariableHeat function (#1)

---
+++ R/variableGenesHeat.R
@@ -0,0 +1,24 @@
+#' @name mostVariableHeat
+#' @description Plots heatmap showing the expression of most variable genes
+#' @param data: normalized deseq data (with varianceStablizing, rlog etc)
+#' @param ntop: how many genes to display (the <ntop> most variable genes)
+#' @param a1: annotation of the samples
+#' @param a2: annotation of the samples
+#' @title mostVariableHeat - heatmap of variable genes
+#' @export mostVariableHeat
+#' @example
+#' vst <- varianceStabilizingTransformation(dds)
+#' mostVariableHeat(data = vst,ntop = 100, a1 = colData$cellLine, a2 = colData$treatment)
+
+mostVariableHeat <- function(data,ntop=50,a1=NULL,a2=NULL) {
+
+  ## Get most variable genes
+  sd <- assay(data)[order(-apply(assay(data),1,sd)),]
+  head(sd)
+
+  df <- as.data.frame(a1=a1,a2=a2)
+
+  pheatmap(sd[1:ntop,],annotation_col = df,cluster_rows = T,show_rownames = T,
+           cluster_cols = T)
+
+}
