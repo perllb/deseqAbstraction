22f1506c410d1533c9b7d1eebe4d4ee36d01c789
perllb <per_ludvik.brattaas@med.lu.se>
2017-11-03 18:31:27 +0100

    show rownames only if ntop <40

--- R/variableGenesHeat.R
+++ R/variableGenesHeat.R
@@ -10,32 +10,36 @@
 #' @export mostVariableHeat
 #' @example
 #' vst <- varianceStabilizingTransformation(dds)
-#' mostVariableHeat(data = vst,ntop = 100, a1 = colData$cellLine, a2 = colData$treatment)
+#' mostVariableHeat(data = vst,ntop = 100, a1 = colData$cellLine, a2 = colData$treatment,n1="Cell Line",n2="Treatment")
 
 mostVariableHeat <- function(data,ntop=50,a1=NULL,a2=NULL,n1=NULL,n2=NULL) {
+
+  #show rownames if 40 or less genes are plotted
+  rowShow <- T
+  if(ntop>40) { rowShow <- F }
 
   if (!is.null(a1) & is.null(a2)) {
 
     df <- data.frame(Var1 = factor(a1))
     rownames(df) <- colnames(data)
     colnames(df) <- n1
-    pheatmap(sd[1:ntop,], annotation_col = df, cluster_rows = T, show_rownames = T, cluster_cols = T)
+    pheatmap(sd[1:ntop,], annotation_col = df, cluster_rows = T, show_rownames = rowShow, cluster_cols = T)
 
   } else if (!is.null(a1) & !is.null(a2)) {
 
     df <- data.frame(Var1 = factor(a1), Var2 = factor(a2))
     rownames(df) <- colnames(data)
     colnames(df) <- c(n1,n2)
-    pheatmap(sd[1:ntop, ], annotation_col = df, cluster_rows = T, show_rownames = T, cluster_cols = T)
+    pheatmap(sd[1:ntop, ], annotation_col = df, cluster_rows = T, show_rownames = rowShow, cluster_cols = T)
 
   } else if (!is.null(a2) & is.null(a1)) {
 
     df <- data.frame(Var1 = factor(a2))
     rownames(df) <- colnames(data)
     colnames(df) <- n2
-    pheatmap(sd[1:ntop, ], annotation_col = df, cluster_rows = T, show_rownames = T, cluster_cols = T)
+    pheatmap(sd[1:ntop, ], annotation_col = df, cluster_rows = T, show_rownames = rowShow, cluster_cols = T)
 
   } else {
-    pheatmap(sd[1:ntop, ], cluster_rows = T, show_rownames = T, cluster_cols = T)
+    pheatmap(sd[1:ntop, ], cluster_rows = T, show_rownames = rowShow, cluster_cols = T)
   }
 }
