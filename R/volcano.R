75e61747f7cd79454b92c4f9e895d3d84f8fc02f
perllb <per_ludvik.brattaas@med.lu.se>
2017-11-06 15:24:10 +0100

    adjust volcano

--- R/volcano.R
+++ R/volcano.R
@@ -11,50 +11,43 @@
 
 volcanoPlot <- function(test,max=NULL,p=.5,title="transcriptome changes") {
 
-  # get values of y axis
+  # get genes with p-adj 0. (these are so significant that there are not a low enough number for the p-adj in R)
+  t <- which(is.infinite(-log10(test$padj)))
+  # give these genes p-adj = the lowest non-zero number in R
+  test[t,]$padj <- min(test[!is.na(test$padj) & test$padj>0,]$padj)
+  # create -log10 ( padj ) yaxis
   yaxis <- -log10(test$padj)
-  # "extremely significant genes with p-adj 0 will get infinite yaxis values.. change to max + .5
-  yaxis[is.infinite(yaxis)] <- max(yaxis) + .5
 
-  # size of dots according to significane
-  cex <- ifelse(test = yaxis>p,yes = .9,no = .4)
-  #color up and down sign..
-  col <- ifelse(test = test$padj<p,
-                   yes=ifelse(test = test$log2FoldChange>0,
-                              yes="firebrick3",
-                              no=ifelse(test$log2FoldChange<0,
-                                        yes = "steelblue4",
-                                        no = "black")),
-                   no = "black")
-  col[is.na(col)] <- "black" ## if NA make sure it's not counted as <p
+  cex <- ifelse(test = yaxis > p, yes = 0.6, no = 0.3)
+  col <- ifelse(test = test$padj < p, yes = ifelse(test = test$log2FoldChange >
+                                                     0, yes = "firebrick3", no = ifelse(test$log2FoldChange <
+                                                                                          0, yes = "steelblue4", no = "black")), no = "black");col[is.na(col)] <- "black"
 
-  # if user puts a max on -log10(padj), adjust plot
-  if(!is.null(max)) {
-    # change y value of points that are above max to y = max + 0.1 instead)
-    yaxis[yaxis>max] <- max+0.1
-    # change point type of those points
-    pchY <- ifelse(test = (yaxis==max+0.1),yes = 18,no = 16)
+  # if user specifies max ylim
+  if (!is.null(max)) {
+    yaxis[yaxis > max] <- max + 0.1
+    pchY <- ifelse(test = (yaxis == max + 0.1), yes = 18,
+                   no = 16)
   }
 
+  # xaxis scaling
   log2FC.max <- max(abs(test[is.finite(test$log2FoldChange),]$log2FoldChange))
 
-  plot(x = test$log2FoldChange,
-        y = yaxis,
-       main=title,
-       col=col,
-       pch=pchY,
-       cex=cex,
-       ylab = "-log10(p-adj)",
-       xlab = "log2FC",
-       xlim = c(-log2FC.max,log2FC.max),
-       ylim = c(0,max(yaxis[is.finite(yaxis)])*1.1))
-  mtext(text = paste("p-adj < ",p,sep=""),side = 3)
+  plot(x = test$log2FoldChange, y = yaxis, main = title, col = col,
+       pch = pchY, cex = cex, ylab = "-log10(p-adj)", xlab = "log2FC",
+       xlim = c(-log2FC.max, log2FC.max), ylim = c(0, max(yaxis[is.finite(yaxis)]) * 1.05))
 
-  sign <- getSign(x = test,p = p,l = 0)
-  u <- nrow(sign$up)
-  d <- nrow(sign$down)
+  # subtitle of plot
+  mtext(text = paste("p-adj < ", p, sep = ""), side = 3)
 
-  legend("topleft",legend = c(paste("up (",u,")",sep=""),paste("down (",d,")",sep = ""),"not significant"),pch=16,col=c("firebrick3","steelblue4","black"),bty='n',cex=.7)
-  abline(v = 0,lty=2)
+  # get sign changed genes
+  sign <- getSignName(x = test, p = p, l = 0)
+  u <- length(sign$up)
+  d <- length(sign$down)
+  legend("topleft", legend = c(paste("up (", u, ")", sep = ""),
+                               paste("down (", d, ")", sep = ""), "not significant"),
+         pch = 16, col = c("firebrick3", "steelblue4", "black"),
+         bty = "n", cex = 0.7)
+  abline(v = 0, lty = 2)
 
 }
