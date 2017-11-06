65578ec8db6fc863bbf444c77c2265385d6ebade
perllb <per_ludvik.brattaas@med.lu.se>
2017-11-06 15:41:38 +0100

    significant genes heatmap

--- R/significantHeat.R
+++ R/significantHeat.R
@@ -1,48 +1,55 @@
-#' @name mostVariableHeat
+#' @name mostSignificantHeat
 #' @description Plots heatmap showing the expression of most variable genes
 #' @param data: normalized deseq matrix data (with varianceStablizing, rlog etc - eg. assay(vsd)
+#' @param test: output from results(dds)
 #' @param ntop: how many genes to display (the <ntop> most variable genes)
 #' @param a1: annotation of the samples
 #' @param a2: annotation of the samples
 #' @param n1: name of annotation in a1
 #' @param n2: name of annotation in a2
-#' @title mostVariableHeat - heatmap of variable genes
-#' @export mostVariableHeat
+#' @title Plot most significant genes: heatmap
+#' @export mostSignificantHeat
 #' @example
 #' vst <- varianceStabilizingTransformation(dds)
-#' mostVariableHeat(data = assay(vst),ntop = 100, a1 = colData$cellLine, a2 = colData$treatment,n1="Cell Line",n2="Treatment")
+#' test <- results(dds)
+#' mostSignificantHeat(data = assay(vst),test=test,ntop = 100, a1 = colData$cellLine, a2 = colData$treatment,n1="Cell Line",n2="Treatment")
 
-mostVariableHeat <- function(data,ntop=50,a1=NULL,a2=NULL,n1=NULL,n2=NULL) {
+mostSignificantHeat <- function(data,test,ntop=50,a1=NULL,a2=NULL,n1=NULL,n2=NULL) {
 
   library(graphics)
-  sd <- data[order(-apply(data,1,sd)),]
-  head(sd)
+
+  ordered <- test[order(-test$padj),]
+  top <- rownames(ordered)
+  merge <- merge(top,data,by.x=1,by.y=0)
+  rownames(merge) <- merge[,1]
+  plotData <- merge[,-1]
+
   #show rownames if 40 or less genes are plotted
   rowShow <- T
-  if(ntop>40) { rowShow <- F }
+  if(ntop>50) { rowShow <- F }
 
   if (!is.null(a1) & is.null(a2)) {
 
     df <- data.frame(Var1 = factor(a1))
     rownames(df) <- colnames(data)
     colnames(df) <- n1
-    pheatmap(sd[1:ntop,], annotation_col = df, cluster_rows = T, show_rownames = rowShow, cluster_cols = T)
+    pheatmap(plot, annotation_col = df, cluster_rows = T, show_rownames = rowShow, cluster_cols = T)
 
   } else if (!is.null(a1) & !is.null(a2)) {
 
     df <- data.frame(Var1 = factor(a1), Var2 = factor(a2))
     rownames(df) <- colnames(data)
     colnames(df) <- c(n1,n2)
-    pheatmap(sd[1:ntop, ], annotation_col = df, cluster_rows = T, show_rownames = rowShow, cluster_cols = T)
+    pheatmap(plotData, annotation_col = df, cluster_rows = T, show_rownames = rowShow, cluster_cols = T)
 
   } else if (!is.null(a2) & is.null(a1)) {
 
     df <- data.frame(Var1 = factor(a2))
     rownames(df) <- colnames(data)
     colnames(df) <- n2
-    pheatmap(sd[1:ntop, ], annotation_col = df, cluster_rows = T, show_rownames = rowShow, cluster_cols = T)
+    pheatmap(plotData, annotation_col = df, cluster_rows = T, show_rownames = rowShow, cluster_cols = T)
 
   } else {
-    pheatmap(sd[1:ntop, ], cluster_rows = T, show_rownames = rowShow, cluster_cols = T)
+    pheatmap(plotData, cluster_rows = T, show_rownames = rowShow, cluster_cols = T)
   }
 }
