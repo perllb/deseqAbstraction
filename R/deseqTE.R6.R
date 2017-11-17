1760a4a8872a2da9fb45c3f7553515109c5b2486
perllb <per_ludvik.brattaas@med.lu.se>
2017-11-17 15:43:48 +0100

    barplot up class fam subfam

--- R/deseqTE.R6.R
+++ R/deseqTE.R6.R
@@ -85,13 +85,153 @@ deseqTE <- R6Class("deseqTE",
 
 
                        df <- t(data.frame(LINE=map.LINE,SINE=map.SINE,LTR=map.LTR,SVA=map.SVA))
-                       colnames(df) <- make.names(names = te$colData$condition,unique = T)
+                       colnames(df) <- make.names(names = self$colData$condition,unique = T)
                        plotPerc <- df*100/tot.map
                        col <- c("darkolivegreen3","indianred4","steelblue","tan4")
                        x <- barplot(plotPerc,ylim = c(0,max(colSums(plotPerc))*1.5),col=col,ylab="% reads mapping TE / mapping to genome",las=2)
                        legend("topleft",legend = rev(c("LINE","SINE","LTR","SVA")),fill=rev(col))
                        title("Percentage of reads mapping to EREs / genome")
 
+                     },
+
+                     upSubFamily = function(p=.9925,l=0,n=10) {
+
+                       # get upregulated elements
+                       up <- getSignName(x = self$test$Default,p = p,l = l)$up
+
+                       # if human genome, include SVAs..
+                       if(self$genome=="hg38") {
+
+                         up.sva <- up[grep("SVA",up)]
+                         sva.up.fam <- substr(x = up.sva,start = 0,stop=5)
+                         up.noSVA <- up[grep("SVA",up,invert = T)]
+                         names <- gsub("\\_.*","",up.noSVA)
+                         names <- c(names,sva.up.fam)
+
+                         RE.features <- self$TE.features[grep("LINE|SINE|LTR|Retroposon",self$TE.features$V2),]
+
+                         tab <- table(names)
+                         tab <- tab[order(-tab)][1:n]
+                         tab.df <- data.frame(names=names(tab),count=tab)
+
+                         # fix color to color family by class
+                         tab.fam <- merge(tab.df,unique(RE.features[,c(1,2)]),by.x=1,by.y=1,all.x=T,sort=F)
+                         col <- ifelse(tab.fam$V2=="LINE",yes="darkolivegreen",
+                                       no=ifelse(tab.fam$V2=="LTR",yes="darkblue",
+                                                 no = ifelse(tab.fam$V2=="Retroposon",yes="red",
+                                                             no=ifelse(tab.fam$V2=="SINE",yes="orange",no="black"))))
+
+                         par(mar=c(8,4,4,4))
+                         x <- barplot(tab,ylim = c(0,max(tab)*1.4),ylab = "Number of upregulated elements",col = col,las = 2)
+                         title(paste("Number of upregulated elements in each family\np-adj<",p,", log2(fc)<",l,sep = ""))
+
+                       } else {
+
+                         RE.features <- self$TE.features[grep("LINE|SINE|LTR",self$TE.features$V2),]
+
+                         tab <- table(names)
+                         tab <- tab[order(-tab)][1:n]
+                         tab.df <- data.frame(names=names(tab),count=tab)
+
+                         # fix color to color family by class
+                         tab.fam <- merge(tab.df,unique(RE.features[,1:2]),by.x=1,by.y=1,all.x=T,sort=F)
+                         col <- ifelse(tab.fam$V2=="LINE",yes="darkolivegreen",
+                                       no=ifelse(tab.fam$V2=="LTR",yes="darkblue",
+                                                 no=ifelse(tab.fam$V2=="SINE",yes="orange",no="black")))
+
+                         par(mar=c(8,4,4,4))
+                         x <- barplot(tab,ylim = c(0,max(tab)*1.4),ylab = "Number of upregulated elements",col = col,las = 2)
+                         title(paste("Number of upregulated elements in each family\np-adj<",p,", log2(fc)<",l,sep = ""))
+
+                       }
+                     },
+
+                     upFamilyBar = function(p=.05,l=0,n=10) {
+
+                       # get upregulated elements
+                       up <- getSignName(x = self$test$Default,p = p,l = l)$up
+
+                       # if human genome, include SVAs..
+                       if(self$genome=="hg38") {
+
+                         up.sva <- up[grep("SVA",up)]
+                         sva.up.fam <- substr(x = up.sva,start = 0,stop=5)
+                         up.noSVA <- up[grep("SVA",up,invert = T)]
+                         names <- gsub("\\_.*","",up.noSVA)
+                         names <- c(names,sva.up.fam)
+
+                         RE.features <- self$TE.features[grep("LINE|SINE|LTR|Retroposon",self$TE.features$V2),]
+                         name.family <- as.character(merge(names,RE.features,by=1)$V3)
+                         tab <- table(name.family)
+                         tab <- tab[order(-tab)][1:n]
+                         tab.df <- data.frame(names=names(tab),count=tab)
+
+                         # fix color to color family by class
+                         tab.fam <- merge(tab.df,unique(RE.features[,2:3]),by.x=1,by.y=2,all.x=T,sort=F)
+                         col <- ifelse(tab.fam$V2=="LINE",yes="darkolivegreen",
+                                       no=ifelse(tab.fam$V2=="LTR",yes="darkblue",
+                                                 no = ifelse(tab.fam$V2=="Retroposon",yes="red",
+                                                             no=ifelse(tab.fam$V2=="SINE",yes="orange",no="black"))))
+
+                         par(mar=c(8,4,4,4))
+                         x <- barplot(tab,ylim = c(0,max(tab)*1.4),ylab = "Number of upregulated elements",col = col,las = 2)
+                         title(paste("Number of upregulated elements in each family\np-adj<",p,", log2(fc)<",l,sep = ""))
+
+                       } else {
+
+                         RE.features <- self$TE.features[grep("LINE|SINE|LTR",self$TE.features$V2),]
+                         name.family <- as.character(merge(names,RE.features,by=1)$V3)
+                         tab <- table(name.family)
+                         tab <- tab[order(-tab)][1:n]
+                         tab.df <- data.frame(names=names(tab),count=tab)
+
+                         # fix color to color family by class
+                         tab.fam <- merge(tab.df,unique(RE.features[,2:3]),by.x=1,by.y=2,all.x=T,sort=F)
+                         col <- ifelse(tab.fam$V2=="LINE",yes="darkolivegreen",
+                                       no=ifelse(tab.fam$V2=="LTR",yes="darkblue",
+                                                 no=ifelse(tab.fam$V2=="SINE",yes="orange",no="black")))
+
+                         par(mar=c(8,4,4,4))
+                         x <- barplot(tab,ylim = c(0,max(tab)*1.4),ylab = "Number of upregulated elements",col = col,las = 2)
+                         title(paste("Number of upregulated elements in each family\np-adj<",p,", log2(fc)<",l,sep = ""))
+
+                       }
+                     },
+
+                     upClassBar = function(p=.05,l=0) {
+
+                       # get upregulated elements
+                       up <- getSignName(x = self$test$Default,p = p,l = l)$up
+
+                       # if human genome, include SVAs..
+                       if(self$genome=="hg38") {
+
+                         up.sva <- up[grep("SVA",up)]
+                         sva.up.fam <- substr(x = up.sva,start = 0,stop=5)
+                         up.noSVA <- up[grep("SVA",up,invert = T)]
+                         names <- gsub("\\_.*","",up.noSVA)
+                         names <- c(names,sva.up.fam)
+
+                         name.class <- as.character(merge(names,self$TE.features,by=1)$V2)
+                         tab <- table(name.class)
+                         tab.RE <- tab[grep("LINE$|SINE$|LTR$|Retroposon$",names(tab))]
+                         names(tab.RE)[names(tab.RE)=="Retroposon"] <- "SVA"
+                         col <- c("darkolivegreen3","indianred4","steelblue","tan4")
+                         x <- barplot(tab.RE,ylim = c(0,max(tab)*1.4),ylab = "Number of upregulated elements",col = col,las = 2)
+                         title(paste("Number of upregulated elements in each class\np-adj<",p,", log2(fc)<",l,sep = ""))
+
+                       } else { ## mouse (no SVA)
+
+                         names <- gsub("\\_.*","",up)
+
+                         name.class <- as.character(merge(names,self$TE.features,by=1)$V2)
+                         tab <- table(name.class)
+                         tab.RE <- tab[grep("LINE$|SINE$|LTR$",names(tab))]
+                         col <- c("darkolivegreen3","indianred4","steelblue")
+                         x <- barplot(tab.RE,ylim = c(0,max(tab)*1.4),ylab = "Number of upregulated elements",col = col,las = 2)
+                         title(paste("Number of upregulated elements in each class\np-adj<",p,", log2(fc)<",l,sep = ""))
+
+                       }
                      },
 
                      maPlotTE = function(testData,c1="cond1",c2="cond2",p=.05,l=0) {
