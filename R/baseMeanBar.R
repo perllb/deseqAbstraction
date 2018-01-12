884d47df50fa4877b584cc0fc0e75b055f775cac
perllb <per_ludvik.brattaas@med.lu.se>
2018-01-12 09:17:02 +0100

    base

--- R/baseMeanBar.R
+++ R/baseMeanBar.R
@@ -19,56 +19,34 @@ baseMeanBar <- function(deseqAbs,genes,cond=NULL) {
   cols <- colorRampPalette(brewer.pal(8, "Greys"))
 
   # if no condition defined, run default
-  if ( is.null(cond) ) {
+  if ( is.null(cond) ) {data <- deseqAbs$baseMean$Mean[,cond]} else {data <- deseqAbs$baseMean$Mean}
 
-    ## make colors
-    mycolors <- cols(length(unique(deseqAbs$colData$condition)))
-    # plot one for each gene 
-    for(gene in genes) {
+  ## make colors
+  mycolors <- ifelse(is.null(cond),yes=cols(length(unique(deseqAbs$colData$condition))),no = cols(length(cond)))
   
-      plot <- deseqAbs$baseMean$Mean[gene,]
-      sd <- deseqAbs$baseMean$SD[gene,]
-      x <- barplot(plot,ylim=c(0,max(plot+sd)*1.25),ylab="mean normalized read counts",col = mycolors,las=2)
-      arrows(x0 = x,y0 = plot,x1 = x,y1 = plot+sd,length = .1,angle = 90)
-      title(main = gene)
+  # plot one for each gene 
+  for(gene in genes) {
+
+    plot <- data[gene,]
+    sd <- data[gene,]
+    x <- barplot(plot,ylim=c(0,max(plot+sd)*1.25),ylab="mean normalized read counts",col = mycolors,las=2)
+    arrows(x0 = x,y0 = plot,x1 = x,y1 = plot+sd,length = .1,angle = 90)
+    title(main = gene)
+
+    ## if more than two conditions, then skip plotting errorbars  
+    if(ncol(data)<3) {
+      if (is.null(cond)) { 
+        padj <- deseqAbs$test$Default[gene,]$padj 
+      } else {
+        padj <- deseqAbs$makeDiffex(c1=cond[1],c2=cond[2])$padj
+      }
   
-      ## if more than two conditions, then skip plotting errorbars  
-      if(ncol(deseqAbs$baseMean$Mean)<3) {
-        padj <- deseqAbs$test$Default[gene,]$padj
-    
-        lab <- ifelse(test = is.na(padj) | padj > .05,yes = "NA",
-                      no = ifelse(test = padj<.0001,yes = "***",
-                                  no = ifelse(test = padj<.01,yes = "**",
-                                              no = ifelse(test = padj<.05,yes = "*",no = "NA"))))
-        arrows(x0 = x[1],y0 = max(plot+sd)*1.1,x1 = x[2],y1 = max(plot+sd)*1.1,code=0)
-        text(x = x[1]+((x[2]-x[1])/2),y = max(plot)*1.2,labels = lab,cex = 1)
-      }
-    }
-  }
-  else {
-    data <- deseqAbs$baseMean$Mean[,cond]
-    ## make colors
-    mycolors <- cols(length(cond))
-    # plot one for each gene 
-    for(gene in genes) {
-      
-      plot <- data[gene,]
-      sd <- data[gene,]
-      x <- barplot(plot,ylim=c(0,max(plot+sd)*1.25),ylab="mean normalized read counts",col = mycolors,las=2)
-      arrows(x0 = x,y0 = plot,x1 = x,y1 = plot+sd,length = .1,angle = 90)
-      title(main = gene)
-      
-      ## if more than two conditions, then skip plotting errorbars  
-      if(ncol(data)<3) {
-        padj <- deseqAbs$test$Default[gene,]$padj
-        
-        lab <- ifelse(test = is.na(padj) | padj > .05,yes = "NA",
-                      no = ifelse(test = padj<.0001,yes = "***",
-                                  no = ifelse(test = padj<.01,yes = "**",
-                                              no = ifelse(test = padj<.05,yes = "*",no = "NA"))))
-        arrows(x0 = x[1],y0 = max(plot+sd)*1.1,x1 = x[2],y1 = max(plot+sd)*1.1,code=0)
-        text(x = x[1]+((x[2]-x[1])/2),y = max(plot+sd)*1.2,labels = lab,cex = 1)
-      }
+      lab <- ifelse(test = is.na(padj) | padj > .05,yes = "NA",
+                    no = ifelse(test = padj<.0001,yes = "***",
+                                no = ifelse(test = padj<.01,yes = "**",
+                                            no = ifelse(test = padj<.05,yes = "*",no = "NA"))))
+      arrows(x0 = x[1],y0 = max(plot+sd)*1.1,x1 = x[2],y1 = max(plot+sd)*1.1,code=0)
+      text(x = x[1]+((x[2]-x[1])/2),y = max(plot)*1.2,labels = lab,cex = 1)
     }
   }
 }
